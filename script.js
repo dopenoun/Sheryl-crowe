daf0b809c13f35a32f4d336044c38a20e2f1d75f
dopenoun <139652420+dopenoun@users.noreply.github.com>
2023-12-18 00:30:51 -0500

    Update script.js
    Gave link to song

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
 document.getElementById('spotifyButton').addEventListener('click', function() {
-    window.location.href = 'https://open.spotify.com/track/yourSpotifyLinkHere';
+    window.location.href = 'https://open.spotify.com/track/3ZpQiJ78LKINrW9SQTgbXd?si=QOvHSzhUTICyV0lpVOjdyA';
 });
